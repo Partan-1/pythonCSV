aa234e7d1d52a9ca22c79b6a3a9aa324fe090243
Partan-1 <77670724+Partan-1@users.noreply.github.com>
2021-04-15 13:40:29 +0200

    Add files via upload

---
+++ CSVwrite.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('employee_file.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['Name', 'Dept', 'Birth_month'])
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+    employee_writer.writerow(['Eric Edward', 'IT', 'April'])
